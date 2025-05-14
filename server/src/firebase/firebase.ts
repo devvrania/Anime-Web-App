bddf678236425824b4fa68b57ae312ec53068985
devvrania <devv.rania@gmail.com>
2025-05-14 22:45:49 +0500

    Node, Next and Firebase configurations

---
+++ server/src/firebase/firebase.ts
@@ -0,0 +1,10 @@
+// server/src/firebase/firebase.ts
+import admin from "firebase-admin";
+import serviceAccount from "./serviceAccountKey.json";
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
+});
+
+export const db = admin.firestore();
+export const auth = admin.auth();
